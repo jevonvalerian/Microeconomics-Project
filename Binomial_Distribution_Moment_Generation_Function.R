5b5aa4f2946a412fe5e43746adfc9586ca2ba032
Hieronimus Jevon Valerian <51231857+jevonvalerian@users.noreply.github.com>
2021-01-08 18:58:02 +0700

    Add files via upload

---
+++ Binomial_Distribution_Moment_Generation_Function.R
@@ -0,0 +1,7 @@
+#1.5.2. SIMULASI
+jumlah_polis_pengaju_klaim <- rbinom(12, 100, 0.24)
+jumlah_polis_pengaju_klaim
+
+#2.4.2. SIMULASI PENAWARAN 10
+jumlah_polis_pengaju_klaim <- rbinom(12, 1, 0.1)
+jumlah_polis_pengaju_klaim 
